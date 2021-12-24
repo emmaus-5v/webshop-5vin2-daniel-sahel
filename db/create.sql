efa7fd8da630625013ade251f885fa2d54b20f1d
DanielRadumilo <112471@emmauscollege.nl>
2021-12-24 16:38:51 +0000

    d

--- db/create.sql
+++ db/create.sql
@@ -27,10 +27,10 @@ insert into products (id, name, description, code, price, typemerk_id) values (5
 insert into products (id, name, description, code, price, typemerk_id) values (1, 'Deluxe Koptelefoon', 'De koptelefoon voor de mensen die houden van chique dingen. De geluidskwaliteit is heel goed en het heeft goede surround sound functies.', '693155505-7', 219.99);
 insert into products (id, name, description, code, price, typemerk_id) values (6, 'Zwarte Koptelefoon', 'Een basic zwarte koptelefoon met redelijke audio kwaliteit', '686928463-6', 19.99, 1);
 insert into products (id, name, description, code, price, typemerk_id) values (3, 'Katoortjes Koptelefoon', 'De Koptelefoon die je hoogswaarschijnlijk met heel veel spijt gaat kopen. Het heeft redelijke audio kwaliteit en het heeft noise suppression.', '492662523-7', 49.99);
-insert into products (id, name, description, code, price, typemerk_id) values
-insert into products (id, name, description, code, price, typemerk_id) values
-insert into products (id, name, description, code, price, typemerk_id) values
-insert into products (id, name, description, code, price, typemerk_id) values
+insert into products (id, name, description, code, price, typemerk_id) values (7, 'Beyerd Koptelefoon', 'Deze koptelefoon heeft een uitstekende stereo geluidskwaliteit en het bevat ook hele goede geluidsdemping.', '5671341443-3', 149.99);
+insert into products (id, name, description, code, price, typemerk_id) values (8, 'Swissten grijze koptelefoon', 'De koptelefoon van het zwitserse merk met een hele goede kwaliteit. Deze koptelefoon is voor de echte genieters van muziek.', 119.99);)
+insert into products (id, name, description, code, price, typemerk_id) values (9, 'Grundig Koptelefoon', 'Deze koptelefoon is van een zeer goed en betrouwbaar Duits merk. De geluidskwaliteit is van middelmatige kwaliteit, maar voor de prijs is het de beste deal', 49.99);
+insert into products (id, name, description, code, price, typemerk_id) values (10, 'Swissten zwarte koptelefoon', 'Een koptelefoon van een zwitsers merk. De kwaliteit is geweldig en het geluid dat je ermee krijgt is ook uitstekend.', 79.99);
 
 CREATE TABLE merken (
   id INTEGER PRIMARY KEY,
